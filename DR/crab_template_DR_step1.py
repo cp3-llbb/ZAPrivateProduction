36a5372440d9e4757d1aeb39eae4c65605b70a66
alesaggio <saggio.alessia@gmail.com>
2019-02-25 14:18:46 +0100

    add cmsdriver and crab scripts

---
+++ DR/crab_template_DR_step1.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_('General')
+config.General.transferLogs = True
+config.General.transferOutputs = True
+config.General.workArea = 'DR_step1_TAG'
+config.General.requestName = 'template'
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.psetName = 'template_DR_step1_cfg.py'
+
+config.section_('Data')
+config.Data.inputDBS = 'phys03'
+config.Data.inputDataset = 'DATASET'
+config.Data.publication = True
+config.Data.unitsPerJob = 1
+config.Data.splitting = 'FileBased'
+config.Data.outputDatasetTag = 'DR_step1_TAG'
+config.Data.outLFNDirBase = '/store/user/asaggio/'
+
+config.section_('Site')
+config.Site.storageSite = 'T2_BE_UCL'
+
+config.section_('User')
+
+config.section_('Debug')
+
